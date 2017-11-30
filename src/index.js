ffc8eb0405f7d00d19319a1458f7d4587869d261
Faraz Syed <github@services.farazsyed.com>
2017-11-29 20:22:21 -0700

    Import Plugin base class directly

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import {
   merge,
   reduce,
 } from 'lodash';
+import Plugin from 'character/plugin';
 import arrify from 'arrify';
 import models from './models';
 import modules from './modules';
@@ -22,134 +23,128 @@ import sessions from './sessions';
 
 const debug = require('debug')('character:authentication');
 
-module.exports = function(Plugin) {
-  return class Authentication extends Plugin {
-    define() {
-      if (!this.config.isValid) {
-        return debug(
-          'Invalid configuration. Not attaching authentication plugin. Fix the configuration and restart the server',
-        );
-      }
-
-      /**
-       * Make sure each authenticator has the following properties:
-       *  - `failureRedirect`
-       *  - `onboardKnownAccounts`
-       *  - `successRedirect`
-       */
-      const authenticators = mapValues(
-        this.config.authenticators,
-        authenticator =>
-          Object.assign(
-            {
-              failureRedirect: this.config.login,
-              onboardKnownAccounts: this.config.onboardKnownAccounts,
-              successRedirect: this.config.successRedirect,
-            },
-            authenticator,
-          ),
+module.exports = class Authentication extends Plugin {
+  define() {
+    if (!this.config.isValid) {
+      return debug(
+        'Invalid configuration. Not attaching authentication plugin. Fix the configuration and restart the server',
       );
-
-      // body parsing is currently enabled on all plugin router routes by `CoreFramework`
-
-      // add session-middleware
-      const { session, sessionMethods } = sessions.setup(
-        this.config,
-        this.database.connection,
-        this.deps.sessionStore,
-      );
-      this.preRouterMiddleware.push(sessionMethods); // adds `req.character.get/set` for safe access of Character session data
-      this.postRouterMiddleware.push(session); // session purposely mounted on `/` for downstream routes, else internal requests to self will generate extra sessions
-
-      // add request methods such as `req.isAuthenticated`
-      this.preRouterMiddleware.push(requests.extend);
-
-      // attach authenticators
-      this.deps.session = session;
-      forEach(modules.load(authenticators), (module, name) => {
-        flow(
-          arrify, // modules may be a single authenticator or an array of authenticators
-          modules =>
-            modules.forEach(Module => {
-              // TODO test ability to return multiple modules from an authenticator
-              const config = merge({}, Module.defaults(), authenticators[name]); // apply the authenticator defaults
-              const module = new Module(
-                name,
-                config,
-                this.database,
-                this.deps,
-                this.events,
-              );
-              // TODO do authenticator modules have root middleware as well? (pre- and post-router middleware)
-              this.router.use(`/${name}`, module.router);
-            }),
-        )(module);
-      });
-    }
-
-    static defaults() {
-      return {
-        base: '/auth',
-        login: '/login',
-        onboardKnownAccounts: true,
-        successRedirect: '/',
-      };
     }
 
     /**
-     * Return models for both this plugin and all authenticators
-     *
-     * @param {Object} config
-     * @return {Object}
+     * Make sure each authenticator has the following properties:
+     *  - `failureRedirect`
+     *  - `onboardKnownAccounts`
+     *  - `successRedirect`
      */
-    static models(config) {
-      const result = reduce(
-        modules.load(config.authenticators),
-        (models, module, authenticatorName) =>
-          assign(
-            {},
-            models,
-            flow(
-              arrify, // modules may be a single authenticator or an array of authenticators
-              modules => modules.map(Module => Module.models()),
-              models => assign({}, ...models),
-              models =>
-                mapKeys(
-                  models,
-                  (model, modelName) => `${authenticatorName}$${modelName}`, // insert the model into the authenticator models namespace
-                ),
-            )(module),
-          ),
-        models,
-      );
-      return result;
-    }
+    const authenticators = mapValues(
+      this.config.authenticators,
+      authenticator =>
+        Object.assign(
+          {
+            failureRedirect: this.config.login,
+            onboardKnownAccounts: this.config.onboardKnownAccounts,
+            successRedirect: this.config.successRedirect,
+          },
+          authenticator,
+        ),
+    );
 
-    static name() {
-      return 'authentication';
-    }
+    // body parsing is currently enabled on all plugin router routes by `CoreFramework`
 
-    static validateConfig(data) {
-      const config = data.plugins.authentication;
-      return and(
-        check(config, 'missing authentication settings'),
-        check(
-          config.authenticators &&
-            Object.keys(config.authenticators).length > 0,
-          'missing authenticators',
+    // add session-middleware
+    const { session, sessionMethods } = sessions.setup(
+      this.config,
+      this.database.connection,
+      this.deps.sessionStore,
+    );
+    this.preRouterMiddleware.push(sessionMethods); // adds `req.character.get/set` for safe access of Character session data
+    this.postRouterMiddleware.push(session); // session purposely mounted on `/` for downstream routes, else internal requests to self will generate extra sessions
+
+    // add request methods such as `req.isAuthenticated`
+    this.preRouterMiddleware.push(requests.extend);
+
+    // attach authenticators
+    this.deps.session = session;
+    forEach(modules.load(authenticators), (module, name) => {
+      flow(
+        arrify, // modules may be a single authenticator or an array of authenticators
+        modules =>
+          modules.forEach(Module => {
+            // TODO test ability to return multiple modules from an authenticator
+            const config = merge({}, Module.defaults(), authenticators[name]); // apply the authenticator defaults
+            const module = new Module(
+              name,
+              config,
+              this.database,
+              this.deps,
+              this.events,
+            );
+            // TODO do authenticator modules have root middleware as well? (pre- and post-router middleware)
+            this.router.use(`/${name}`, module.router);
+          }),
+      )(module);
+    });
+  }
+
+  static defaults() {
+    return {
+      base: '/auth',
+      login: '/login',
+      onboardKnownAccounts: true,
+      successRedirect: '/',
+    };
+  }
+
+  /**
+   * Return models for both this plugin and all authenticators
+   *
+   * @param {Object} config
+   * @return {Object}
+   */
+  static models(config) {
+    const result = reduce(
+      modules.load(config.authenticators),
+      (models, module, authenticatorName) =>
+        assign(
+          {},
+          models,
+          flow(
+            arrify, // modules may be a single authenticator or an array of authenticators
+            modules => modules.map(Module => Module.models()),
+            models => assign({}, ...models),
+            models =>
+              mapKeys(
+                models,
+                (model, modelName) => `${authenticatorName}$${modelName}`, // insert the model into the authenticator models namespace
+              ),
+          )(module),
         ),
-        ...Object.keys(config.authenticators).map(name =>
-          validateAuthenticator(name, config.authenticators[name]),
-        ),
-        check(
-          config.session.cookie.maxAge,
-          'missing session cookie maximum age',
-        ),
-        check(config.session.keys, 'missing session secret keys'),
-      );
-      // TODO check that config.successRedirect exists or that each authenticator has a successRedirect
-    }
-  };
+      models,
+    );
+    return result;
+  }
+
+  static name() {
+    return 'authentication';
+  }
+
+  static validateConfig(data) {
+    const config = data.plugins.authentication;
+    return and(
+      check(config, 'missing authentication settings'),
+      check(
+        config.authenticators && Object.keys(config.authenticators).length > 0,
+        'missing authenticators',
+      ),
+      ...Object.keys(config.authenticators).map(name =>
+        validateAuthenticator(name, config.authenticators[name]),
+      ),
+      check(config.session.cookie.maxAge, 'missing session cookie maximum age'),
+      check(config.session.keys, 'missing session secret keys'),
+    );
+    // TODO check that config.successRedirect exists or that each authenticator has a successRedirect
+  }
 };
 
 /**
